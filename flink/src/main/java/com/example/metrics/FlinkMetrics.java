1c0d8ffd7ff0e7c6e67b4273ef620e00f1b7b53e
wufeiye1 <wufeiye1@jd.com>
2024-12-10 15:12:28 +0800

    add flink module

---
+++ flink/src/main/java/com/example/metrics/FlinkMetrics.java
@@ -0,0 +1,41 @@
+package com.example.metrics;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.metrics.Counter;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class FlinkMetrics {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        //设置WebUI绑定的本地端口
+        conf.setString(RestOptions.BIND_PORT,"8081");
+        //使用配置
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+        env.setParallelism(1);
+        DataStreamSource<String> source = env.socketTextStream("localhost", 9999);
+        SingleOutputStreamOperator<String> map = source.map(new MyMapper());
+        map.print();
+        env.execute();
+    }
+}
+
+class MyMapper extends RichMapFunction<String, String> {
+    private transient Counter counter;
+
+    @Override
+    public void open(Configuration config) {
+        this.counter = getRuntimeContext()
+                .getMetricGroup()
+                .counter("myCounter");
+    }
+
+    @Override
+    public String map(String value) throws Exception {
+        this.counter.inc();
+        return value;
+    }
+}
